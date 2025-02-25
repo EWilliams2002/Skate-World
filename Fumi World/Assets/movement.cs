8179419882b0a51e8e825fc504a85a354b5a4904
Evan Williams <157436047+EWilliams2002@users.noreply.github.com>
2025-02-25 14:33:42 -0800

    test 2

--- Fumi World/Assets/movement.cs
+++ Fumi World/Assets/movement.cs
@@ -5,7 +5,7 @@ public class movement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // hello test #2
     }
 
     // Update is called once per frame
